3cffbc27b4d4d676f43c34894da6e07561a8eeed
Md Nuruddin <nuruddinalways99@gmail.com>
2023-06-09 20:15:25 +0600

    Navigation updated

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -4,149 +4,146 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState(true);
 
   return (
-    <div>
-      <nav className='bg-white'>
-        <div className='max-w-7xl mx-auto md:px-0 px-5'>
-          <div className='flex justify-between'>
-            <div className='flex space-x-7'>
-              <div>
-                {/* Website Logo */}
-                <Link to='/' className='flex items-center py-4 '>
-                  <span className='font-bold text-[#49BBBD] text-xl'>
-                    Edoofy
-                  </span>
-                </Link>
-              </div>
-              {/* Primary Navbar items */}
-              <div className='hidden md:flex items-center space-x-1'>
-                <Link
-                  to='/'
-                  className='py-2 px-2 text-gray-500 border-b-4 border-transparent hover:border-b-4 hover:border-[#49BBBD]'
-                >
-                  Home
-                </Link>
-                <Link
-                  to='/instructors'
-                  className='py-2 px-2 text-gray-500 border-b-4 border-transparent hover:border-b-4 hover:border-[#49BBBD]'
-                >
-                  Instructors
-                </Link>
-                <Link
-                  to='/classes'
-                  className='py-2 px-2 text-gray-500 border-b-4 border-transparent hover:border-b-4 hover:border-[#49BBBD]'
-                >
-                  Classes
-                </Link>
-              </div>
+    <nav className='bg-white shadow-lg'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex justify-between h-16'>
+          <div className='flex items-center'>
+            {/* Website Logo */}
+            <Link to='/' className='flex items-center'>
+              <span className='text-2xl font-bold text-[#49BBBD]'>Edoofy</span>
+            </Link>
+          </div>
+          <div className='flex items-center space-x-4'>
+            {/* Primary Navbar items */}
+            <div className='hidden md:flex items-center space-x-4'>
+              <Link
+                to='/'
+                className='text-gray-500 hover:text-[#49BBBD] px-3 py-2 text-md font-medium'
+              >
+                Home
+              </Link>
+              <Link
+                to='/instructors'
+                className='text-gray-500 hover:text-[#49BBBD] px-3 py-2 text-md font-medium'
+              >
+                Instructors
+              </Link>
+              <Link
+                to='/classes'
+                className='text-gray-500 hover:text-[#49BBBD] px-3 py-2 text-md font-medium'
+              >
+                Classes
+              </Link>
             </div>
             {/* Secondary Navbar items */}
-            <div className='hidden md:flex items-center space-x-3 '>
+            <div className='hidden md:flex items-center space-x-4'>
               {/* Conditional rendering of Dashboard and Login button */}
-              {user === true ? (
+              {user ? (
                 <>
                   <Link
                     to='/dashboard'
-                    className='py-2 px-2 font-medium text-gray-500 rounded hover:bg-blue-500 hover:text-white transition duration-300'
+                    className='text-gray-500 hover:bg-blue-500 hover:text-white px-3 py-2 text-md font-medium rounded'
                   >
                     Dashboard
                   </Link>
-                  <FaUserCircle size={40} />
+                  <FaUserCircle size={24} />
                 </>
               ) : (
                 <Link
                   to='/login'
-                  className='py-2 px-4 font-medium text-white bg-[#49BBBD] rounded hover:bg-blue-400 transition duration-300'
+                  className='text-white bg-[#49BBBD] hover:bg-blue-400 transition duration-300 px-4 py-2 text-md font-medium rounded'
                 >
                   Login
                 </Link>
               )}
             </div>
             {/* Mobile menu button */}
-            <div className='md:hidden flex items-center'>
+            <div className='-mr-2 flex md:hidden'>
               <button
-                className='outline-none mobile-menu-button'
+                className='text-gray-500 hover:text-blue-500 inline-flex items-center justify-center p-2 rounded-md focus:outline-none'
                 onClick={() => setIsOpen(!isOpen)}
               >
                 <svg
-                  className=' w-6 h-6 text-gray-500 hover:text-blue-500 '
-                  x-show='!showMenu'
+                  className={`${isOpen ? "hidden" : "block"} w-6 h-6`}
+                  xmlns='http://www.w3.org/2000/svg'
                   fill='none'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
                   viewBox='0 0 24 24'
                   stroke='currentColor'
                 >
-                  <path d='M4 6h16M4 12h16M4 18h16'></path>
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M4 6h16M4 12h16M4 18h16'
+                  />
+                </svg>
+                <svg
+                  className={`${isOpen ? "block" : "hidden"} w-6 h-6`}
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M6 18L18 6M6 6l12 12'
+                  />
                 </svg>
               </button>
             </div>
           </div>
         </div>
-        {/* Mobile Menu */}
-        <div
-          className={`${
-            isOpen ? "" : "hidden"
-          } mobile-menu md:hidden bg-white shadow-lg`}
-        >
-          <ul className=''>
-            <li className='active'>
-              <Link
-                to='/'
-                className='block text-sm px-2 py-4 text-white bg-blue-500 font-semibold'
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/instructors'
-                className='block text-sm px-2 py-4 hover:bg-blue-500 transition duration-300 font-semibold'
-              >
-                Instructors
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/classes'
-                className='block text-sm px-2 py-4 hover:bg-blue-500 transition duration-300 font-semibold'
-              >
-                Classes
-              </Link>
-            </li>
-
-            {user === false ? (
-              <>
-                <li>
-                  <Link
-                    to='/dashboard'
-                    className='block text-sm px-2 py-4 hover:bg-blue-500 transition duration-300 font-semibold'
-                  >
-                    Dashboard
-                  </Link>
-                </li>
-
-                <li>
-                  <FaUserCircle size={40} />
-                </li>
-              </>
-            ) : (
-              <li>
-                <Link
-                  to='/login'
-                  className='block text-sm px-2 py-4 text-white bg-blue-500 font-semibold'
-                >
-                  Login
-                </Link>
-              </li>
-            )}
-          </ul>
+      </div>
+      {/* Mobile Menu */}
+      <div className={`${isOpen ? "block" : "hidden"} md:hidden bg-white`}>
+        <div className='px-2 pt-2 pb-3 space-y-1'>
+          <Link
+            to='/'
+            className='block text-sm px-3 py-2 text-gray-500 font-medium hover:text-[#49BBBD]'
+          >
+            Home
+          </Link>
+          <Link
+            to='/instructors'
+            className='block text-sm px-3 py-2 text-gray-500 font-medium hover:text-[#49BBBD]'
+          >
+            Instructors
+          </Link>
+          <Link
+            to='/classes'
+            className='block text-sm px-3 py-2 text-gray-500 font-medium hover:text-[#49BBBD]'
+          >
+            Classes
+          </Link>
         </div>
-      </nav>
-    </div>
+        <div className='px-2 py-3 space-y-1'>
+          {/* Conditional rendering of Dashboard and Login button */}
+          {user ? (
+            <>
+              <Link
+                to='/dashboard'
+                className='block text-sm px-3 py-2 text-gray-500 font-medium hover:bg-blue-500 hover:text-white rounded'
+              >
+                Dashboard
+              </Link>
+              <FaUserCircle size={24} />
+            </>
+          ) : (
+            <Link
+              to='/login'
+              className='block text-sm px-3 py-2 text-white bg-[#49BBBD] hover:bg-blue-400 transition duration-300 font-medium rounded'
+            >
+              Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 };
 
